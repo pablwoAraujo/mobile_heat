4a045ad3f3c7efe76ba80bcfa018fe6f184379b7
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2021-10-21 20:38:39 -0300

    Initial settings

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.BLACK_SECONDARY,
+  }
+});
