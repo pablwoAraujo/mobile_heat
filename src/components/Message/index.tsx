e4bf19c6c6f8c45e08d153bcdc3b5df5301d4792
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2021-10-21 23:12:09 -0300

    Animation in messages added with Moti

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { MotiView } from 'moti';
 
 import { UserPhoto } from '../UserPhoto';
 import { styles } from './styles';
@@ -20,7 +21,12 @@ type Props = {
 
 export function Message({ data }: Props){
   return (
-    <View style={styles.container}>
+    <MotiView 
+      style={styles.container}
+      from={{ opacity:0, translateY:-50 }}
+      animate={{ opacity:1, translateY:0 }}
+      transition={{ type:'timing', duration:700 }}
+    >
       <Text style={styles.message}>
         {data.text}
       </Text>
@@ -35,6 +41,6 @@ export function Message({ data }: Props){
           {data.user.login}
         </Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
